a76d4b5282a2aa3bb29e5fceebdc2579e3e4cdf7
igor <igor.ramos.c@hotmail.com>
2023-11-13 19:16:17 -0500

    verification of token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js"
 import bvrypt from "bcryptjs";
 import {createAccessToken} from "../libs/jwt.js"
+import  jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
 
 export const register = async (req, res) =>{
     const {email, password, username} = req.body 
@@ -73,4 +75,29 @@ export const profile = async (req,res) => {
         createdAt: userFound.createdAt,
         updatedAt: userFound.updatedAt,
     });
+}
+
+export const verifyToken = async (req, res) =>{
+    const {token} = req.cookies
+    if(!token) return res.status(401).json({
+        message: "Unautorized"
+    });
+
+    jwt.verify(token,TOKEN_SECRET , async (err, user) =>{
+        if(err) return res.status(401).json({
+            message: "Unautorized"
+        });
+
+        const userFound = await User.findById(user.id);
+
+        if(!userFound) return res.status(401).json({
+            message: "Unautorized"
+        });
+
+        return res.json({
+            id: userFound.id,
+            username: userFound.username,
+            email: userFound.email,
+        })
+    })
 }
