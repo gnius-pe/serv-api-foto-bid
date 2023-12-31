236d87fcdf9200792c713739f992af98923dc04a
igor <igor.ramos.c@hotmail.com>
2023-12-31 18:19:18 -0500

    mas detalles en la doc

--- src/libs/swagger.js
+++ src/libs/swagger.js
@@ -4,7 +4,26 @@ import swaggerUI from 'swagger-ui-express';
 const options = {
     definition : {
         openapi:"3.0.0",
-        info : { title: 'API SERV FOTO BID', version:"1.0.0"}
+        info : { 
+            title: 'API SERV FOTO BID', 
+            version:"1.0.0", 
+            description: 'Una API para gestionar fotos y usuarios',
+            contact: {
+                name: 'Igor Ramos Cruzado',
+                email: 'igor.ramos.c@hotmail.com',
+            },
+            license: {
+                name: 'Licencia de la API',
+                url: 'https://url-de-tu-licencia.com',
+            },
+            servers: [
+                {
+                  url: 'http://localhost:3000', // URL de tu servidor
+                  description: 'Servidor local',
+                },
+                // Puedes agregar más servidores si tu API está desplegada en varios lugares
+            ],
+        },
     },
     apis: ['src/routes/*.js','../models/user.model.js'],
 };
