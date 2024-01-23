85a35725f6d07371830bf45acd1b52fc91aceac5
igor <igor.ramos.c@hotmail.com>
2024-01-23 16:13:03 -0500

    new atribute in foto

--- src/models/foto.model.js
+++ src/models/foto.model.js
@@ -2,6 +2,36 @@ import mongoose, { mongo } from "mongoose";
 
 import mongoosePaginate from "mongoose-paginate-v2"
 
+const Like = new mongoose.Schema({
+        user:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        }
+    });
+    
+
+const Comment = new mongoose.Schema({
+        author:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        },
+        content:{
+            type: String,
+            required: true
+        }
+    });
+    
+
 const FotoInformationSchema = new mongoose.Schema({
     image:{
         public_id : String,
@@ -10,17 +40,24 @@ const FotoInformationSchema = new mongoose.Schema({
     description:{
         type: String
     },
-    like:{
-        type: Number
-    },
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
+    },
+    comments:{
+        type: [Comment],
+        required: false
+    },
+    likes:{
+        type: [Like],
+        required: false
     }
+    
 },{
     timestamps:true
 });
+
 
 FotoInformationSchema.plugin(mongoosePaginate);
 export default mongoose.model("FotoInformation", FotoInformationSchema);
